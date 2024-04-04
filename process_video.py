06934da9290406fd755d518011bd4c8121235855
jg_mac <gentinetta.joris@gmail.com>
2024-04-04 17:28:41 -0400

    - updated visualization - implemented hand cropping - vectorized out of plane calculations

--- process_video.py
+++ process_video.py
@@ -5,7 +5,7 @@ import cv2
 import imageio
 
 import mediapipe as mp
-from mediapipe.python.solutions import pose
+from mediapipe.python.solutions import pose, hands
 import numpy as np
 from math import sqrt, ceil
 import pandas as pd
@@ -16,65 +16,95 @@ from tqdm import tqdm
 from helpers.utils import MergingHelper, AnglesHelper
 from helpers.visualization import Visualization
 
+import matplotlib.pyplot as plt
 
-
-def run_mediapipe(cap, frames, video_timestamps, intact_hand=None):
+#todo use full models
+def run_mediapipe(cap, frames, video_timestamps, intact_hand=None, scales = None):
     body_model_path = 'models/mediapipe/pose_landmarker.task'
     hands_model_path = 'models/mediapipe/hand_landmarker.task'
     BaseOptions = mp.tasks.BaseOptions
     PoseLandmarker = mp.tasks.vision.PoseLandmarker
     PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
     VisionRunningMode = mp.tasks.vision.RunningMode
-    mergingHelper = MergingHelper()
 
     options = mp.tasks.vision.HandLandmarkerOptions(base_options=BaseOptions(model_asset_path=hands_model_path)
                                                     , num_hands=2, running_mode=VisionRunningMode.VIDEO)
-    hands_model = mp.tasks.vision.HandLandmarker.create_from_options(options)
+    hand_models = {'Right': mp.tasks.vision.HandLandmarker.create_from_options(options), 'Left': mp.tasks.vision.HandLandmarker.create_from_options(options)}
 
     options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=body_model_path),
         running_mode=VisionRunningMode.VIDEO)
     body_model = PoseLandmarker.create_from_options(options)
 
-    body_cols = mergingHelper.map['Body'].keys()
-    right_hand_cols = mergingHelper.map['Right'].keys()
-    left_hand_cols = mergingHelper.map['Left'].keys()
-
+    hand_cols = hands.HandLandmark._member_names_ + ['BODY_WRIST', 'ELBOW', 'SHOULDER', 'HIP']
+    body_cols = pose.PoseLandmark._member_names_
     body_columns = pd.MultiIndex.from_product([['Body'], body_cols, ['x', 'y', 'z']])
-    right_hand_columns = pd.MultiIndex.from_product([['Right'], right_hand_cols, ['x', 'y', 'z']])
-    left_hand_columns = pd.MultiIndex.from_product([['Left'], left_hand_cols, ['x', 'y', 'z']])
+    right_hand_columns = pd.MultiIndex.from_product([['Right'], hand_cols, ['x', 'y', 'z']])
+    left_hand_columns = pd.MultiIndex.from_product([['Left'], hand_cols, ['x', 'y', 'z']])
 
     columns = body_columns.append(right_hand_columns).append(left_hand_columns)
     joints_df = pd.DataFrame(index=frames, columns=columns)
 
-    frame_id = 0
-    while cap.isOpened():
+    roi_half_size = 100
+    print("MediaPipe processing...")
+    for frame_id in tqdm(frames):
         success, frame = cap.read()
         if not success:
             print("Finished MediaPipe processing.")
             break
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        body_results = body_model.detect_for_video(mp_image, int(1000 * video_timestamps[frame_id])).pose_landmarks
+        if len(body_results) == 0:
+            frame_id += 1
+            continue
 
-        body_results = body_model.detect_for_video(mp_image, int(1000 * video_timestamps[frame_id]))
-        hands_results = hands_model.detect_for_video(mp_image, int(1000 * video_timestamps[frame_id]))
-        pose_landmarks = mergingHelper.mergeLandmarks(hands_results, body_results, prior=intact_hand)
+        for landmark_name in body_cols:
+            joints_df.loc[frame_id, ('Body', landmark_name, 'x')] = int(body_results[0][pose.PoseLandmark[landmark_name]].x * scales[0])
+            joints_df.loc[frame_id, ('Body', landmark_name, 'y')] = int(body_results[0][pose.PoseLandmark[landmark_name]].y * scales[1])
+            joints_df.loc[frame_id, ('Body', landmark_name, 'z')] = int(body_results[0][pose.PoseLandmark[landmark_name]].z * scales[2])
 
-        coordinates = [[lmk.x, lmk.y, lmk.z] for lmk in pose_landmarks.landmark]
-        coordinates = np.asarray(coordinates)
-        cols = {'Body': body_cols, 'Right': right_hand_cols, 'Left': left_hand_cols}
-        for key, value in cols.items():
-            pass
-            for landmark_name in value:
-                joints_df.loc[frame_id, (key, landmark_name, 'x')] = coordinates[
-                mergingHelper.map[key][landmark_name], 0]
-                joints_df.loc[frame_id, (key, landmark_name, 'y')] = coordinates[
-                mergingHelper.map[key][landmark_name], 1]
-                joints_df.loc[frame_id, (key, landmark_name, 'z')] = coordinates[
-                mergingHelper.map[key][landmark_name], 2]
+        for side in ['Right', 'Left']:
+            wrist = [joints_df.loc[frame_id, ('Body', f'{side.upper()}_WRIST', 'x')], joints_df.loc[frame_id, ('Body', f'{side.upper()}_WRIST', 'y')]]
+            x_start = max(0, wrist[0] - roi_half_size)
+            x_end = min(scales[0], wrist[0] + roi_half_size)
+            y_start = max(0, wrist[1] - roi_half_size)
+            y_end = min(scales[1], wrist[1] + roi_half_size)
+            cropped_frame = frame[y_start:y_end, x_start:x_end]
+            rgb_cropped_frame = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
 
+            plt.imshow(rgb_cropped_frame)
+            plt.show()
 
-        frame_id += 1
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_cropped_frame)
+
+            hands_results = hand_models[side].detect_for_video(mp_image, int(1000 * video_timestamps[frame_id])).hand_landmarks
+            if len(hands_results) == 0:
+                continue
+            elif len(hands_results) == 1:
+                hand_id = 0
+
+            else:
+                x = joints_df.loc[frame_id, ('Body', f'{side.upper()}_WRIST', 'x')]
+                y = joints_df.loc[frame_id, ('Body', f'{side.upper()}_WRIST', 'y')]
+                target = np.array([x, y])
+
+                candidates = np.zeros(2)
+                x = x_start + hands_results[0][hands.HandLandmark['WRIST']].x * (x_end - x_start)
+                y = y_start + hands_results[0][hands.HandLandmark['WRIST']].y * (y_end - y_start)
+                candidates[0] = np.linalg.norm(target - np.array([x, y]))
+
+                x = x_start + hands_results[1][hands.HandLandmark['WRIST']].x * (x_end - x_start)
+                y = y_start + hands_results[1][hands.HandLandmark['WRIST']].y * (y_end - y_start)
+                candidates[1] = np.linalg.norm(target - np.array([x, y]))
+                hand_id = np.argmin(candidates)
+
+            for landmark_name in hands.HandLandmark._member_names_:
+                joints_df.loc[frame_id, (side, landmark_name, 'x')] = x_start + hands_results[hand_id][hands.HandLandmark[landmark_name]].x * (x_end - x_start)
+                joints_df.loc[frame_id, (side, landmark_name, 'y')] = y_start + hands_results[hand_id][hands.HandLandmark[landmark_name]].y * (y_end - y_start)
+                joints_df.loc[frame_id, (side, landmark_name, 'z')] = x_start + hands_results[hand_id][hands.HandLandmark[landmark_name]].z * (x_end - x_start)
+
+        # frame_id += 1
 
     return joints_df
 
@@ -84,8 +114,8 @@ def update_left_right(joints_df):
         for landmark_name in ['SHOULDER', 'ELBOW', 'HIP']:
             joints_df.loc[:, idx[key, landmark_name, slice(None)]] = joints_df.loc[:, idx['Body', f'{value}_{landmark_name}', slice(None)]].values
 
-        joints_df.loc[:, idx[key, 'BODY_WRIST', slice(None)]] = joints_df.loc[:,
-                                                                   idx['Body', f'{value}_WRIST', slice(None)]].values
+        joints_df.loc[:, idx[key, 'BODY_WRIST', ['x', 'y']]] = joints_df.loc[:, idx[key, 'WRIST', ['x', 'y']]].values
+        joints_df.loc[:, idx[key, 'BODY_WRIST', 'z']] = joints_df.loc[:, idx['Body', f'{value}_WRIST', 'z']].values
     return joints_df
 
 
@@ -95,6 +125,8 @@ if __name__ == "__main__":
     parser.add_argument('--experiment_name', type=str, required=True, help='Experiment name')
     parser.add_argument('--visualize', action='store_true', help='Visualize the output')
     parser.add_argument('--intact_hand', type=str, default=None, help='Intact hand')
+    # argument called plane_frames that takes a list of integers:
+    parser.add_argument('--plane_frames', nargs='+', type=int, default=range(20), help='Frames to calculate the limb lenghts')
     args = parser.parse_args()
 
     experiment_dir = join(args.data_dir, 'experiments', args.experiment_name)
@@ -106,74 +138,60 @@ if __name__ == "__main__":
     n_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     print(f"Number of frames: {n_frames}")
 
-    frames = range(0, int(n_frames))
-    mediapipe_landmark_names = pose.PoseLandmark._member_names_
-    video_timestamps = np.load(join(experiment_dir, "cropped_timestamps.npy"))
-    joints_df = run_mediapipe(cap, frames, video_timestamps, args.intact_hand)
-
     vid = imageio.get_reader(input_video_path, 'ffmpeg')
     fps_in = vid.get_meta_data()['fps']
     vid_size = vid.get_meta_data()['size']
-    x_scale, y_scale = vid_size
-    z_scale = x_scale
+    scales = (vid_size[0], vid_size[1], vid_size[0])
+    frames = range(0, int(n_frames))
+    mediapipe_landmark_names = pose.PoseLandmark._member_names_
 
-    idx = pd.IndexSlice
-    joints_df.loc[:, idx[:, :, 'x']] = joints_df.loc[:, idx[:, :, 'x']] * x_scale
-    joints_df.loc[:, idx[:, :, 'y']] = joints_df.loc[:, idx[:, :, 'y']] * y_scale
-    joints_df.loc[:, idx[:, :, 'z']] = joints_df.loc[:, idx[:, :, 'z']] * z_scale
+    video_timestamps = np.load(join(experiment_dir, "cropped_timestamps.npy"))
+    joints_df = run_mediapipe(cap, frames, video_timestamps, args.intact_hand, scales)
     cap.release()
 
-    # joints_df = interpolate_missing_joints(joints_df, frames)
+    joints_df = update_left_right(joints_df)
     joints_df.to_parquet(join(experiment_dir, "mediapipe_output.parquet"))
 
-    joints_df = pd.read_parquet(join(experiment_dir, "mediapipe_output.parquet"))
-
-    # get upper arm length and forearm length for the first 20 frames:
+    # get upper arm length and forearm length:
     upper_arm_lengths = []
     forearm_lengths = []
-    sides = [args.intact_hand.upper()] if args.intact_hand else ['RIGHT', 'LEFT']
-    for i in range(2):
+    sides = [args.intact_hand] if args.intact_hand else ['Right', 'Left']
+
+    for i in args.plane_frames:
         for side in sides:
-            shoulder = joints_df.loc[i, ('Body', f'{side}_SHOULDER', ['x', 'y'])].values
-            elbow = joints_df.loc[i, ('Body', f'{side}_ELBOW', ['x', 'y'])].values
-            wrist = joints_df.loc[i, ('Body', f'{side}_WRIST', ['x', 'y'])].values
+            shoulder = joints_df.loc[i, (side, 'SHOULDER', ['x', 'y'])].values
+            elbow = joints_df.loc[i, (side, 'ELBOW', ['x', 'y'])].values
+            wrist = joints_df.loc[i, (side, 'WRIST', ['x', 'y'])].values
 
             upper_arm_lengths.append(np.linalg.norm(shoulder - elbow))
             forearm_lengths.append(np.linalg.norm(elbow - wrist))
     average_upper_arm_length = np.mean(np.array(upper_arm_lengths))
     average_forearm_length = np.mean(np.array(forearm_lengths))
 
-    for i in range(len(joints_df.index)):
-        # shoulder = joints_df.loc[i, idx['Body', 'RIGHT_SHOULDER', slice(None)]].values - joints_df.loc[i, idx['Body', 'LEFT_SHOULDER', slice(None)]].values
-        # spine = joints_df.loc[i, idx['Body', 'HIPS', slice(None)]].values - joints_df.loc[i, idx['Body', 'CHEST', slice(None)]].values
-        # body_normal = np.cross(shoulder, spine)
-        # body_normal = body_normal / np.linalg.norm(body_normal)
-        for side in ['RIGHT', 'LEFT']:
-            upper_arm = joints_df.loc[i, idx['Body', f'{side}_ELBOW', slice(None)]].values - joints_df.loc[i, idx['Body', f'{side}_SHOULDER', slice(None)]].values
-            forearm = joints_df.loc[i, idx['Body', f'{side}_WRIST', slice(None)]].values - joints_df.loc[i, idx['Body', f'{side}_ELBOW', slice(None)]].values
+    print('Correcting Z values...')
+    for side in sides:
+        # Calculate upper_arm and forearm for all rows at once
+        upper_arm = joints_df.loc[:, idx[side, 'ELBOW', slice(None)]].values - joints_df.loc[:,
+                                                                                idx[side, 'SHOULDER', slice(None)]].values
+        forearm = joints_df.loc[:, idx[side, 'WRIST', slice(None)]].values - joints_df.loc[:,
+                                                                              idx[side, 'ELBOW', slice(None)]].values
+        upper_arm = upper_arm.astype(np.float64)
+        forearm = forearm.astype(np.float64)
 
-            # upper_arm_z_sign = np.sign(np.dot(upper_arm, body_normal))
-            # forearm_z_sign = np.sign(np.dot(upper_arm, body_normal))
-            upper_arm_z_sign = -1
-            forearm_z_sign = -1
+        # Calculate missing_len for upper_arm and forearm for all rows at once
+        missing_len_upper_arm = average_upper_arm_length ** 2 - upper_arm[:, 0] ** 2 - upper_arm[:, 1] ** 2
+        missing_len_forearm = average_forearm_length ** 2 - forearm[:, 0] ** 2 - forearm[:, 0] ** 2
 
-            missing_len = average_upper_arm_length**2 - upper_arm[0]**2 - upper_arm[1]**2
-            if missing_len > 0:
-                upper_arm[2] = sqrt(missing_len)
-            else:
-                upper_arm[2] = 0
+        # Use np.where to conditionally assign sqrt(missing_len) or 0 to upper_arm[2] and forearm[2]
+        upper_arm[:, 2] = np.where(missing_len_upper_arm > 0, np.sqrt(missing_len_upper_arm), 0)
+        forearm[:, 2] = np.where(missing_len_forearm > 0, np.sqrt(missing_len_forearm), 0)
 
-            missing_len = average_forearm_length ** 2 - forearm[0] ** 2 - forearm[1] ** 2
-            if missing_len > 0:
-                forearm[2] = sqrt(missing_len)
-            else:
-                forearm[2] = 0
+        joints_df.loc[:, idx['Body', f'{side.upper()}_ELBOW', 'z']] = joints_df.loc[:, idx[
+                                                                                           'Body', f'{side.upper()}_SHOULDER', 'z']].values + upper_arm[
+                                                                                                                                              :, 2] * -1
+        joints_df.loc[:, idx['Body', f'{side.upper()}_WRIST', 'z']] = joints_df.loc[:, idx[
+                                                                                           'Body', f'{side.upper()}_ELBOW', 'z']].values + forearm[:, 2] * -1
 
-            joints_df.loc[i, idx['Body', f'{side}_ELBOW', 'z']] = joints_df.loc[i, idx['Body', f'{side}_SHOULDER', 'z']] + upper_arm[2] * upper_arm_z_sign
-            joints_df.loc[i, idx['Body', f'{side}_WRIST', 'z']] = joints_df.loc[i, idx['Body', f'{side}_ELBOW', 'z']] + forearm[2] * forearm_z_sign
-            # todo update LR
-            # todo scaling depending on elbow z value (relative to shoulder / plane)
-            pass
     joints_df = update_left_right(joints_df)
     joints_df.to_parquet(join(experiment_dir, "corrected.parquet"))
 
@@ -189,7 +207,7 @@ if __name__ == "__main__":
 
 
 
-    # corrected = pd.read_parquet(join(experiment_dir, "corrected.parquet"))
-    # anglesHelper = AnglesHelper()
-    # angles_df = anglesHelper.getArmAngles(corrected)
-    # angles_df.to_parquet(join(experiment_dir, "angles.parquet"))
+    corrected = pd.read_parquet(join(experiment_dir, "corrected.parquet"))
+    anglesHelper = AnglesHelper()
+    angles_df = anglesHelper.getArmAngles(corrected)
+    angles_df.to_parquet(join(experiment_dir, "angles.parquet"))
