31e19dda7417ab9696db04f35bb2ce6dc784d116
jg_mac <gentinetta.joris@gmail.com>
2024-05-01 23:48:59 -0400

    implemented external camera/emg sync

--- preprocess_data.py
+++ preprocess_data.py
@@ -32,12 +32,55 @@ def crop_video(cap, data_dir, out_dir, start_frame, end_frame, x_start, x_end, y
     cap.release()
     out.release()
 
-    timestamps = np.load(os.path.join(data_dir, 'video_timestamps.npy'))
+    timestamps = np.load(os.path.join(data_dir, 'cropped_video_timestamps.npy'))
 
     if end_frame == -1:
         end_frame = frame_count - 1
     timestamps = timestamps[start_frame:end_frame + 1]
     np.save(join(out_dir, f'cropped_timestamps.npy'), timestamps)
+
+def trigger_crop(cap, data_dir):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+
+    output_path = join(data_dir, f'video_cropped.mp4')
+    out = cv2.VideoWriter(output_path, fourcc, cap.get(cv2.CAP_PROP_FPS), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+
+    frame_count = 0
+    found_trigger = False
+    record_frame = False
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        print(frame_count)
+        print(frame[0, frame.shape[1]//2, :])
+
+
+        if frame[0, frame.shape[1]//2, 0] >= 200 and frame[0, frame.shape[1]//2, 1] >= 200 and not found_trigger:
+            found_trigger = True
+
+            plt.imshow(frame)
+            plt.title(f'Trigger, frame {frame_count}')
+            plt.show()
+            if record_frame:
+                break
+
+        if found_trigger and frame[0, frame.shape[1]//2, 0] < 200 and frame[0, frame.shape[1]//2, 1] < 200:
+            record_frame = True
+
+        if record_frame:
+            out.write(frame)
+
+        frame_count += 1
+
+    cap.release()
+    out.release()
+
+
+
 
 
 def show_frame(cap, frame_number):
@@ -88,9 +131,16 @@ if __name__ == '__main__':
     parser.add_argument('--y_end', type=int, default=-1, help='End y coordinate for cropping')
     parser.add_argument('--start_frame', type=int, default=0, help='Start frame for cropping')
     parser.add_argument('--end_frame', type=int, default=-1, help='End frame for cropping')
+    parser.add_argument('--trigger_channel', type=int, default=0, help='Trigger channel')
+    parser.add_argument('--trigger_value', type=int, default=200, help='Trigger value')
     args = parser.parse_args()
 
     cap = cv2.VideoCapture(join(args.data_dir, 'video.mp4'))
+
+    if not os.path.exists(join(args.data_dir, 'video_cropped.mp4')):
+        trigger_crop(cap, args.data_dir)
+
+    cap = cv2.VideoCapture(join(args.data_dir, 'video_cropped.mp4'))
 
     if args.crop:
         x_start = int(args.x_start)
@@ -117,6 +167,34 @@ if __name__ == '__main__':
         if os.path.exists(out_dir):
             raise ValueError(f'Output directory {out_dir} already exists.')
         os.makedirs(out_dir, exist_ok=True)
+
+        emg = np.load(join(args.data_dir, 'emg.npy'))
+        emg_timestamps = np.load(join(args.data_dir, 'emg_timestamps.npy'))
+        video_timestamps = np.zeros((int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+        found_trigger = False
+        record_frame = False
+        start_time = -1
+        end_time = -1
+        for i, signal in enumerate(emg[:, args.trigger_channel]):
+            if signal >= args.trigger_value:
+                found_trigger = True
+                if record_frame:
+                    end_time = emg_timestamps[i]
+                    break
+
+            if found_trigger and signal < args.trigger_value:
+                record_frame = True
+                start_time = emg_timestamps[i]
+
+        assert start_time != -1 and end_time != -1, 'Could not find trigger in EMG data.'
+
+        for i in range(len(video_timestamps)):
+            video_timestamps[i] = start_time + (end_time - start_time) * i / len(video_timestamps)
+
+        np.save(join(out_dir, 'cropped_video_timestamps.npy'), video_timestamps)
+
+
 
         crop_video(cap, args.data_dir, out_dir, start_frame=args.start_frame, end_frame=args.end_frame, x_start=x_start,
                    x_end=x_end, y_start=y_start,
