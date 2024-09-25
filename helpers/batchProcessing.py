c126cab705f9b1b386f0e0c1003ae18b0f1a9ae5
mikey-fernandez2 <mfernan@mit.edu>
2024-09-25 16:16:38 -0400

    End of trials + demo 09/25 - with some processing scripts added

--- helpers/batchProcessing.py
+++ helpers/batchProcessing.py
@@ -11,31 +11,10 @@ def run_command(command):
 def main():
     # List of commands to run with different arguments
     commands = [
-        "python s3_process_video.py --data_dir data/P7_453/recordings/thumbFlEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/thumbAbAd --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/indexFlEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/mrpFlEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/fingersFlEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/handOpCl --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/pinchOpCl --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/pointOpCl --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/keyOpCl --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/indexFlDigitsEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/freeMovement1 --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/freeMovement2 --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/freeMovement3 --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process",
-        # "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 230 --video_end 2030 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlEx --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 750 --video_end 2550 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlHandCl --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 235 --video_end 2035 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlHandOp --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 340 --video_end 2140 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlEx_up --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 750 --video_end 2550 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlHandCl_up --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 235 --video_end 2035 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/wristFlHandOp_up --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 340 --video_end 2140 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/boxBlocksFree --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 175 --video_end 1985 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/boxBlocksActual --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 240 --video_end 2040 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/mugGrasp --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 270 --video_end 2070 --process",
-        "python s3_process_video.py --data_dir data/P7_453/recordings/scissorGrip --experiment_name 1  --intact_hand Right --plane_frames_start 10 --plane_frames_end 110 --video_start 270 --video_end 2070 --process",
-
+        "python s3_process_video.py --data_dir data/P6_820/recordings/midFlEx --experiment_name 1  --intact_hand Left --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process --jorisThumb",
+        "python s3_process_video.py --data_dir data/P6_820/recordings/ringFlEx --experiment_name 1  --intact_hand Left --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process --jorisThumb",
+        "python s3_process_video.py --data_dir data/P6_820/recordings/pinkyFlEx --experiment_name 1  --intact_hand Left --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 1810 --process --jorisThumb",
+        "python s3_process_video.py --data_dir data/P6_820/recordings/fingerWave --experiment_name 1  --intact_hand Left --plane_frames_start 10 --plane_frames_end 110 --video_start 10 --video_end 3610 --process --jorisThumb"
     ]
 
     # Create a pool of processes
